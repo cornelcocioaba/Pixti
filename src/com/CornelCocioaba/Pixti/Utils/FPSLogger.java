1e463279590340ca9a73f91ce7e3a5cf7beaecb7
Cornel Cocioaba <cornel.cocioaba@yahoo.com>
2014-01-28 23:55:05 +0200

    Added FPSLogger

---
+++ src/com/CornelCocioaba/Pixti/Utils/FPSLogger.java
@@ -0,0 +1,42 @@
+package com.CornelCocioaba.Pixti.Utils;
+
+import com.CornelCocioaba.Pixti.Core.Time;
+import com.CornelCocioaba.Pixti.GameObject.IUpdateable;
+
+public class FPSLogger implements IUpdateable {
+	private long mLastTime;
+	private int nbFrames;
+	private int mFPS;
+	private float mFrequencyInv;
+
+	
+	public FPSLogger() {
+		this(1);
+	}
+	
+	/**
+	 * @param frequency times per second
+	 */
+	public FPSLogger(float frequency){
+		mLastTime = System.currentTimeMillis();
+		nbFrames = 0;
+		mFPS = 60;
+		mFrequencyInv = 1.0f / frequency;
+	}
+
+
+	@Override
+	public void Update() {
+		nbFrames++;
+		long currentTime = System.currentTimeMillis();
+		if (currentTime - mLastTime >= mFrequencyInv * Time.MILLISECONDS_PER_SECOND) {
+			mFPS = nbFrames;
+			nbFrames = 0;
+			mLastTime = currentTime;
+		}
+	}
+
+	public int getFPS() {
+		return mFPS;
+	}
+}
